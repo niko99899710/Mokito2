a295e1f3f642acb7c56a049b9998caca9af152b2
Karina <bagdasarova-k@inbox.ru>
2023-07-17 01:38:57 +0500

    mokito2

---
+++ src/main/java/com/example/mokito2/Mokito2Application.java
@@ -0,0 +1,13 @@
+package com.example.mokito2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mokito2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mokito2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mokito2/Mokito2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mokito2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mokito2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
